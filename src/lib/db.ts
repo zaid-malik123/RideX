ae0f9e10bffb7154e43256f322b025a5af1ffbc2
Zaid Malik <mzaidmalik25@gmail.com>
2026-04-21 22:21:53 +0530

    db connection function created

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,5 +6,38 @@ const db_Url = process.env.MONGO_URL;
 
 if(!db_Url) throw new Error("data base url not found");
 
+let cached = global.mongoose;
 
-console.log("THIS IS THE DB URL ", db_Url)
+if(!cached) {
+
+    cached = global.mongoose = {
+        conn: null,
+        promise: null
+    }
+}
+
+export const connectDb = async () => {
+
+    if(cached.conn) {
+        return cached.conn;
+    }
+
+    if(!cached.promise) {
+        cached.promise = mongoose.connect(db_Url).then(c => c.connection)
+    }
+
+
+    try {
+
+        const conn = await cached.promise;
+        return conn;
+        
+    } catch (error: unknown) {
+
+        console.log(error)
+
+        if(error instanceof Error) {
+            throw new Error("Db connection time error ", error)
+        }
+    }
+}

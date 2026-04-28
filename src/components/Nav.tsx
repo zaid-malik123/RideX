f4f0df353ca5be1ff932ed9984b602445fc9dc1d
Zaid Malik <mzaidmalik25@gmail.com>
2026-04-28 20:49:37 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+import { motion } from "motion/react";
+import Image from "next/image";
+import logo from "../../public/logo.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavItems = ["Home", "Bookings", "Contact Us", "About Us"];
 
 const Nav = () => {
+  const pathName = usePathname()
   return (
-    <div>Nav</div>
-  )
-}
+    <motion.div
+      initial={{ y: -60, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      className={`fixed top-3 left-1/2 -translate-x-1/2 w-[94%] md:w-[86%] z-50 rounded-full bg-[#0B0B0B] text-white shadow-xl shadow-[0_15px_50px_rgba(0,0,0,0.7)] py-3`}
+    >
+      <div className="max-w-7xl mx-auto px-4 md:px-8 flex items-center justify-between">
+        <Image src={logo} height={44} width={44} priority alt="logo" />
+      </div>
 
-export default Nav
+      <div>
+        {NavItems.map((item, idx) => {
+          const href = `/${item.toLowerCase()}`
+          const active = href == pathName
+          return  <Link className={`text-sm font-medium transition ${
+            active ? "text-white" : "text-gray-400 hover:text-white"
+          }`} key={idx} href={href}>
+            {item}
+          </Link>
+        })}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Nav;

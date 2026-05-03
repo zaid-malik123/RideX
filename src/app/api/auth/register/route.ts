89053eaffa0c6ad43e12fce0fd50d12449a26f4b
Zaid Malik <mzaidmalik25@gmail.com>
2026-05-03 10:54:33 +0530

    feat: enhance user registration by validating input fields and sending OTP email

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -6,6 +6,7 @@ import { handleError } from "@/lib/handleError";
 import { ApiResponse } from "@/lib/ApiResponse";
 import { ApiError } from "@/lib/ApiError";
 import { generateOtp } from "@/utils/generateOtp";
+import { sendOtpEmail } from "@/lib/mails/sendOtpMail";
 
 export async function POST(req: NextRequest) {
   try {
@@ -13,56 +14,56 @@ export async function POST(req: NextRequest) {
 
     const { name, email, password } = await req.json();
 
-    const user = await userModel.findOne({ email });
+    if (!name || !email || !password) {
+      throw new ApiError("All fields are required", 400);
+    }
+
+    if (password.length < 6) {
+      throw new ApiError("Password must be atleast 6 char", 400);
+    }
+
+    let user = await userModel.findOne({ email });
 
     if (user && user.isVerifiedEmail) {
       throw new ApiError("User already exists", 400);
     }
 
     const otp = generateOtp();
-    const otpExp = new Date(Date.now() + 10 * 60 * 1000)
+    const otpExp = new Date(Date.now() + 10 * 60 * 1000);
+    const hash = await bcrypt.hash(password, 10);
 
-    if(password.length < 6) {
-      throw new ApiError("Password must be atleast 6 char ", 400)
-    }
-    const hash = await bcrypt.hash(password, 10)
-
-    if(user && !user.isVerifiedEmail) {
+    if (user && !user.isVerifiedEmail) {
       user.name = name;
-      user.email = email;
       user.password = hash;
       user.otp = otp;
-      user.otpExpiresAt = otpExp
+      user.otpExpiresAt = otpExp;
 
       await user.save();
+    } else {
+      user = await userModel.create({
+        name,
+        email,
+        password: hash,
+        otp,
+        otpExpiresAt: otpExp,
+      });
     }
 
-    else {
-      const createdUser = await userModel.create({
-          name,
-          email,
-          password: hash,
-          otp,
-          otpExpireAt: otpExp
-      })
-
-      await sendMail(
-        to: createdUser.email,
-        subj
-      )
+    try {
+      await sendOtpEmail(user.email, otp);
+    } catch (err) {
+      console.log("Email failed", err);
     }
-
-
 
     return ApiResponse({
       success: true,
-      message: "User created",
-      data: createdUser,
-      status: 201,
+      message: "OTP sent to email",
+      data: user,
+      status: 200,
     });
 
   } catch (error) {
-    console.log("register api error ", error)
+    console.log("register api error", error);
     return handleError(error);
   }
 }

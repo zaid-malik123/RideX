cbe89d349f219846bcb714278f989068cd380a20
Zaid Malik <mzaidmalik25@gmail.com>
2026-05-05 16:41:15 +0530

    feat: implement proxy middleware for route authentication and authorization

---
+++ src/proxy.ts
@@ -0,0 +1,65 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
+
+const PUBLIC_ROUTES = [
+    "/"
+]
+
+const PUBLIC_APIS = [
+    "/api/auth/*"
+]
+
+export async function proxy(req: NextRequest) {
+
+    const { pathname } = req.nextUrl
+
+    if(
+        pathname.startsWith("/_next") ||
+        pathname.startsWith("/favicon.ico") ||
+        pathname.startsWith(".")
+    ) {
+        return NextResponse.next()
+    }
+
+    if(PUBLIC_ROUTES.includes(pathname)) {
+        return NextResponse.next()
+    }
+
+    if(PUBLIC_APIS.includes(pathname)) {
+        return NextResponse.next()
+    }
+
+    const session = await auth();
+
+    if(!session) {
+        return NextResponse.redirect( new URL("/", req.url))
+    }
+
+    const role = session.user.role;
+
+    if(pathname.startsWith("/admin")){
+        if(role != "admin") {
+            return NextResponse.redirect( new URL("/", req.url))
+        }
+    }
+
+    if(pathname.startsWith("/partner")){
+        if(role != "partner") {
+            return NextResponse.redirect( new URL("/", req.url))
+        }
+    }
+
+    if(pathname.startsWith("/api")){
+        if(!session.user) {
+            return Response.json({
+                message: "Unauthorized"
+            }, {status: 401})
+        }
+    }
+    
+    return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};

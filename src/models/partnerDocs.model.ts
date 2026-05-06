e96c91734dce378583fd3922a26b21fc5b9c48a4
Zaid Malik <mzaidmalik25@gmail.com>
2026-05-06 20:22:19 +0530

    feat: add PartnerDocs model with schema definition and types

---
+++ src/models/partnerDocs.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { Document } from "mongoose";
+
+interface IPartnerDocs extends Document {
+  owner: mongoose.Types.ObjectId;
+  aadharUrl: string;
+  rcUrl: string;
+  licenseUrl: string;
+  status: "approved" | "pending" | "rejected";
+  rejectionReason?: string;
+}
+
+const partnerDocsSchema = new mongoose.Schema<IPartnerDocs>(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    aadharUrl: {
+      type: String,
+      required: true,
+    },
+    rcUrl: {
+      type: String,
+      required: true,
+    },
+    licenseUrl: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["approved", "pending", "rejected"],
+      default: "pending",
+    },
+    rejectionReason: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const PartnerDocs =
+  mongoose.models.PartnerDocs ||
+  mongoose.model<IPartnerDocs>("PartnerDocs", partnerDocsSchema);
+
+export default PartnerDocs;

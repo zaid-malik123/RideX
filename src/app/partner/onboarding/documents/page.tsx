20f8ccafa3e73766a88fc190db9eca4a080909d7
Zaid Malik <mzaidmalik25@gmail.com>
2026-05-07 21:24:59 +0530

    feat: enhance PartnerDocuments component with document upload options and layout improvements

--- src/app/partner/onboarding/documents/page.tsx
+++ src/app/partner/onboarding/documents/page.tsx
@@ -1,10 +1,97 @@
-"use client"
-import { motion } from "motion/react"
+"use client";
+import { ArrowLeft, FileCheck, UploadCloud } from "lucide-react";
+import { motion } from "motion/react";
+import { useRouter } from "next/navigation";
 
 const PartnerDocuments = () => {
+  const router = useRouter();
   return (
-    <div>PartnerDocuments</div>
-  )
-}
+    <div className="min-h-screen bg-white flex items-center justify-center px-4">
+      <motion.div
+        initial={{ opacity: 0, y: 28 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        className="w-full max-w-xl bg-white rounded-3xl border border-gray-300 shadow-[0_25px_70px_rgba(0,0,0,0.15) p-6 sm:p-8]"
+      >
+        <div className="relative text-center">
+          <button
+            onClick={() => router.back()}
+            className="absolute left-0 top-0 w-9 h-9 rounded-full border border-gray-200 flex items-center justify-center hover:bg-gray-100 transition"
+          >
+            <ArrowLeft size={18} />
+          </button>
+          <p className="text-xs text-gray-500 font-medium"> step 2 of 3</p>
+          <h1 className="text-2xl font-bold mt-1">Upload Documents</h1>
+          <p className="text-sm text-gray-500 mt-2">
+            Required for Verification
+          </p>
+        </div>
 
-export default PartnerDocuments
+        <div className="mt-8 space-y-5">
+          <motion.label
+            whileHover={{ scale: 1.03 }}
+            className="flex items-center justify-between p-4 rounded-2xl border border-gray-200 cursor-pointer hover:border-black transition"
+          >
+            <div>
+              <p className="text-sm font-semibold">Aadhar / ID Proof </p>
+              <p className="text-xs text-gray-500">Goverment Issud ID</p>
+            </div>
+            <div>
+              <span className="text-xs text-gray-400">Upload</span>
+              <div className="w-10 h-10 rounded-full bg-black text-white flex items-center justify-center">
+                <UploadCloud size={18} />
+              </div>
+            </div>
+          </motion.label>
+
+          <motion.label
+            whileHover={{ scale: 1.03 }}
+            className="flex items-center justify-between p-4 rounded-2xl border border-gray-200 cursor-pointer hover:border-black transition"
+          >
+            <div>
+              <p className="text-sm font-semibold">Driving License </p>
+              <p className="text-xs text-gray-500">Valid Driving License</p>
+            </div>
+            <div>
+              <span className="text-xs text-gray-400">Upload</span>
+              <div className="w-10 h-10 rounded-full bg-black text-white flex items-center justify-center">
+                <UploadCloud size={18} />
+              </div>
+            </div>
+          </motion.label>
+
+          <motion.label
+            whileHover={{ scale: 1.03 }}
+            className="flex items-center justify-between p-4 rounded-2xl border border-gray-200 cursor-pointer hover:border-black transition"
+          >
+            <div>
+              <p className="text-sm font-semibold">Vehicle RC </p>
+              <p className="text-xs text-gray-500">Registration Certificate</p>
+            </div>
+            <div>
+              <span className="text-xs text-gray-400">Upload</span>
+              <div className="w-10 h-10 rounded-full bg-black text-white flex items-center justify-center">
+                <UploadCloud size={18} />
+              </div>
+            </div>
+          </motion.label>
+        </div>
+
+        <div className="mt-6 flex items-center gap-3 text-xs text-gray-500">
+            <FileCheck size={16} className="mt-[0.5]"/>
+            <p> Documents are securely stored and manually verified by our team .</p>
+        </div>
+
+        <motion.button
+        whileHover={{ scale: 1.02}}
+        whileTap={{ scale: 0.97}}
+        className="mt-8 w-full h-14 rounded-2xl bg-black text-white font-semibold flex items-center justify-center disabled:opacity-40 transition"
+        >
+            Continue
+        </motion.button>
+      </motion.div>
+    </div>
+  );
+};
+
+export default PartnerDocuments;
